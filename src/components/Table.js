ef5f97b7d8cf2a5851311464369ddb94d08cb520
Stephanie Yung <Stephanie.Yung43@myhunter.cuny.edu>
2021-03-13 18:17:39 -0500

    AddRow and AddColumn squares with 0 start +buttons

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,22 +6,46 @@ class Table extends Component {
   constructor() {
     super();
     this.state = {
-      numRows: 1,
-      numCols: 1,
+      numRows: 0,
+      numCols: 0,
       selectedColor: "red"
     }
   }
 
   addRow = () => {
-    this.setState(state => {
-        return {numRows: state.numRows + 1}
-    });
+    let addedRows = this.state.numRows + 1;
+    let cols = this.state.numCols;
+
+    if(cols === 0){
+      this.setState(state => {
+          return {
+            numRows: addedRows,
+            numCols: state.numCols + 1 }
+      });
+    }
+    else{
+      this.setState(state => {
+          return {numRows: state.numRows + 1}
+      });
+    }
   }
 
   addColumn = () => {
-    this.setState(state => {
-        return {numCols: state.numCols + 1}
-    });
+    let addedCols = this.state.numCols + 1;
+    let rows = this.state.numRows;
+
+    if(rows === 0){
+      this.setState(state => {
+          return {
+            numRows: state.numRows + 1,
+            numCols: addedCols }
+      });
+    }
+    else{
+      this.setState(state => {
+          return {numCols: state.numCols + 1}
+      });
+    }
   }
 
   handleColorChange = (event) => {
@@ -43,6 +67,11 @@ class Table extends Component {
       <div>
         <button onClick={this.addRow}>Add Row</button>
         <button onClick={this.addColumn}>Add Column</button>
+        <button onClick={this.removeRow}>Remove Row</button>
+        <button onClick={this.removeColumn}>Remove Column</button>
+        <button onClick={this.fillUncolored}>Fill Uncolored Cells</button>
+        <button onClick={this.fillAll}>Fill All</button>
+        <button onClick={this.clear}>Clear</button>
         <select onChange={this.handleColorChange}>
           <option value="red">red</option>
           <option value="blue">blue</option>
